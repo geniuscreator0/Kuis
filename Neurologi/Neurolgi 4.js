54265005e439829329d3e6fe1475fb15d434c211
geniuscreator0 <108791554+geniuscreator0@users.noreply.github.com>
2022-08-07 16:36:52 +0900

    Create Neurolgi 4.js

---
+++ Neurologi/Neurolgi 4.js
@@ -0,0 +1,10 @@
+var time=60;myApp=[{     question:'Pertanyaan',     options:['Option1','Option2','Option3','Obtion4','Option5'],     answer:0 },
+                   {     question:'Pertanyaan',     options:['Option1','Option2','Option3','Obtion4','Option5'],     answer:0 },
+                   {     question:'Pertanyaan',     options:['Option1','Option2','Option3','Obtion4','Option5'],     answer:0 },
+                   {     question:'Pertanyaan',     options:['Option1','Option2','Option3','Obtion4','Option5'],     answer:0 },
+                   {     question:'Pertanyaan',     options:['Option1','Option2','Option3','Obtion4','Option5'],     answer:0 },
+                   {     question:'Pertanyaan',     options:['Option1','Option2','Option3','Obtion4','Option5'],     answer:0 },
+                   {     question:'Pertanyaan',     options:['Option1','Option2','Option3','Obtion4','Option5'],     answer:0 },
+                   {     question:'Pertanyaan',     options:['Option1','Option2','Option3','Obtion4','Option5'],     answer:0 },
+                   {     question:'Pertanyaan',     options:['Option1','Option2','Option3','Obtion4','Option5'],     answer:0 },
+                   {     question:'Pertanyaan',     options:['Option1','Option2','Option3','Obtion4','Option5'],     answer:0 }]
