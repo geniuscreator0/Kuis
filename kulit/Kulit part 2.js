26eeb5d83556276c88f78cf265f2277e64c42211
geniuscreator0 <108791554+geniuscreator0@users.noreply.github.com>
2022-08-09 18:00:10 +0900

    Create Kulit part 2.js

---
+++ kulit/Kulit part 2.js
@@ -0,0 +1,17 @@
+var time=60;myApp=[{     question:'Seorang wanita 32 tahun datang dengan keluhan muncul plenting-plenting di bibir kanan sejak 3 hari yang lalu. Pasien mengeluh plenting tersebut terasa perih dan seperti terbakar. 3 bulan yang lalu pasien juga pernah mengalami hal serupa. Pada pemeriksaan fisik ditemukan vesikel bergerombol di ujung bibir kanan. Apakah terapi yang dapat diberikan pada pasien tsb?',     options:['Acyclovir 5x800mg selama 5 hari','Acyclovir 5x200mg selama 7 hari','Acyclovir 5x200mg selama 10 hari','Acyclovir 5x200mg selama 5 hari','Acyclovir 3x400mg selama 7 hari'],     answer:3 },
+{     question:'Seorang anak 4 th dibawa ibunya ke poliklinik dengan keluhan muncul bintil-bintil kecil berwarna seperti kulit normal yang menyebar di seluruh badan. Ibu mengatakan sebelumnya pasien demam tinggi 40oC, lemah, nyeri kepala dan nyeri tenggorokan 3 hari lalu. Pada pemeriksaan ditemukan papul homogen di area wajah, extremitas, palmo-plantar dan sedikit di trunkus. Permukaan papul membentuk kubah dengan umbilikasi. Apakah etiologi dari kasus tersebut?',     options:['HHV tipe 2','Pox virus','HSV tipe 1','HPV tipe 6 dan 11','VZV'],     answer:1 },
+{     question:'Seorang anak usia 5 th dibawa ibunya berobat ke poli umum PKM dengan keluhan demam sejak 5 hari ini, batuk, pilek dan mata merah berair disertai muncul bercak merah di badan yang bermula di area belakang kepala. Pada pemeriksaan fisik tampak sakit sedang, sesak nafas, N=120x.m, RR 30x/m, suhu badan 38,5 oC. Pada pemeriksaan dermatologis ditemukan ruam makulopapular eritematosa generalisata, serta ditemukan bercak putih di mulut area buccal. Apakah diagnosis pasien tersebut?',     options:['Variola','Varicella','Hand Foot and Mouth Disease','Morbili','Rubella'],     answer:3 },
+{     question:'Seorang anak 6 tahun datang dengan keluhan muncul plenting-plenting diseluruh tubuhnya terutama didada dan punggungnya. Sebelumnya pasien mengeluh batuk, pilek, dan disertai demam. Pada pemeriksaan fisik ditemukan BP 90/60, HR 111x, RR23x, Tax 38, BB20kg. status lokalis kulit ditemukan lesi polimorfik yang tersebar generalisata. Apakah terapi yang tepat diberikan pada pasien?',     options:['Acyclovir 4x400mg selama 5 hari','Acyclovir 3x400mg selama 7 hari','Acyclovir 4x400mg selama 7 hari','Acyclovir 4x400mg selama 15 hari','Acyclovir 5x400mg selama 7 hari'],     answer:2 },
+{     question:'Seorang wanita usia 45 tahun datang dengan keluhan nyeri pada kaki kanan saat berjalan. Keluhan tersebut dirasakan sejak 7 hari yang lalu. Pada pemeriksaan fisik ditemukan papul yang teraba keras, berwarna kuning, ditengahnya terdapat suatu bintik. Apakah diagnosa pada kasus tersebut?x`',     options:['Veruka vulgaris','Veruka plantaris','Veruka plana juvenilis','Clavus','Moluskum kontagiosum'],     answer:3 },
+{     question:'Pertanyaan',     options:['Option1','Option2','Option3','Obtion4','Option5'],     answer:0 },
+{     question:'Pertanyaan',     options:['Option1','Option2','Option3','Obtion4','Option5'],     answer:0 },
+{     question:'Pertanyaan',     options:['Option1','Option2','Option3','Obtion4','Option5'],     answer:0 },
+{     question:'Pertanyaan',     options:['Option1','Option2','Option3','Obtion4','Option5'],     answer:0 },
+{     question:'Pertanyaan',     options:['Option1','Option2','Option3','Obtion4','Option5'],     answer:0 }]
+
+
+A. 
+B. 
+C. 
+D. 
+E. 
