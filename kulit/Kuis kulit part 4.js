fbf1670a94986d3116a2103e6233c72072038359
geniuscreator0 <108791554+geniuscreator0@users.noreply.github.com>
2022-08-10 11:31:32 +0900

    Update Kuis kulit part 4.js

--- kulit/Kuis kulit part 4.js
+++ kulit/Kuis kulit part 4.js
@@ -1,6 +1,6 @@
 var time=60;myApp=[{     question:'Seorang anak usia 5 tahun datang diantar ibunya ke praktek dokter pribadi dengan keluhan timbul keropeng di wajahnya 5 hari ini. Keluhan disertai rasa gatal namun tidak didapatkan demam pada pasien. Dari pemeriksaan fisik tanda vital dalam batas normal, di area sekitar hidung dan mulut didapatkan lesi berupa vesikel dan krusta multiple berwarna kuning keemasan seperti tetes madu dengan dasar eritema. Apakah hasil pemeriksaan penunjang yang mungkin didapatkan dari kasus tersebut?',     options:['Keratinocyte ballooning','Bakteri berbentuk bulat berjajar seperti rantai berwana biru keunguan','Bakteri berbentuk bulat menggerombol warna biru keunguan','Diplococcus bacteria','Pseudohifa'],     answer:1 },
 {     question:'Seorang anak usia 7 tahun dibawa oleh orang tuanya ke puskesmas dengan keluhan timbul beberapa gelembung di area tangan dan kakinya sejak 4 hari terakhir. Dari pemeriksaan fisik tanda- tanda vital dalam batas normal, didapatkan lesi berupa vesikel dan bula dengan dinding kendur berisi cairan serosa, sebagian sudah pecah dan meninggalkan skuama anular dengan bagian tengah eritem (kolaret) berwarna kecoklatan. Apakah diagnosis kasus tersebut?',     options:['Impetigo krustosa','Impetigo bulosa','Herpes simplex','Varicella','Hand foot and mouth disease'],     answer:1 },
-{     question:'Pertanyaan',     options:['Option1','Option2','Option3','Obtion4','Option5'],     answer:0 },
+{     question:'Seorang laki-laki usia 20 tahun datang ke puskesmas dengan keluhan koreng pada kaki sejak 2 hari yang lalu. 1 minggu yang lalu, timbul bintil kemerahan yang berubah menjadi bisul kemudian pecah mengeluarkan nanah. Dari pemeriksaan status dermatologikus regio dorsum pedis sinistra tampak ulkus dangkal dengan krusta tebal berwarna kuning keabuan. Apakah tatalaksana yang tepat untuk kasus pasien ini ?',     options:['kompres dengan sol. permanganas kalikus 1: 5000','kompres dengan sol. Ringer Asetat','kompres dengan sol. Ringer Lactate','Neomycin cream','Clindamycin cream'],     answer:0 },
 {     question:'Pertanyaan',     options:['Option1','Option2','Option3','Obtion4','Option5'],     answer:0 },
 {     question:'Pertanyaan',     options:['Option1','Option2','Option3','Obtion4','Option5'],     answer:0 },
 {     question:'Pertanyaan',     options:['Option1','Option2','Option3','Obtion4','Option5'],     answer:0 },
@@ -9,13 +9,3 @@ var time=60;myApp=[{     question:'Seorang anak usia 5 tahun datang diantar ibun
 {     question:'Pertanyaan',     options:['Option1','Option2','Option3','Obtion4','Option5'],     answer:0 },
 {     question:'Pertanyaan',     options:['Option1','Option2','Option3','Obtion4','Option5'],     answer:0 }]
 
-Seorang laki-laki usia 20 tahun datang ke puskesmas dengan keluhan koreng pada kaki sejak 2 hari yang lalu. 1 minggu yang lalu, timbul bintil kemerahan yang berubah menjadi
-bisul kemudian pecah mengeluarkan nanah. Dari pemeriksaan status dermatologikus
-regio dorsum pedis sinistra tampak ulkus dangkal dengan krusta tebal berwarna kuning
-keabuan. Apakah tatalaksana yang tepat untuk kasus pasien ini ?
-
-A. kompres dengan sol. permanganas kalikus 1: 5000
-B. kompres dengan sol. Ringer Asetat
-C. kompres dengan sol. Ringer Lactate
-D. Neomycin cream
-E. Clindamycin cream
